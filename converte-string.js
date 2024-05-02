502e5f8ca21a788126a5861d66ccfaf6c70768d8
Viniciusss065 <gomesfigueiredo@gmail.com>
2024-05-02 16:25:37 -0400

    exemplo de converte-string em js

---
+++ converte-string.js
@@ -0,0 +1,4 @@
+1 let x = "A resposta é " + 42;
+2 let y = 42 + " é a resposta";
+3 console.log(x); //Saida: "A resposta é 42"
+4 console.log(y); //Saida: "42 é a resposta"
---
+++ tipo-dados.js
@@ -0,0 +1,13 @@
+1   let nome = "Wanderson";
+2   let verdadeiro = true;
+3   let falso = false;
+4   let numero = 40;
+5   let reais 25.99;
+6   let nulο = null;
+7   let indefinido;
+8   let NotANumber = 0 / 0;
+9   let objeto = {
+10     name: "Wanderson",
+11     age: 40,
+12     Profession: "Teacher"
+13  }
