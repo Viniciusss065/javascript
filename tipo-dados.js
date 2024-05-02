f072cad212275440a45247aefd8efbb2c517fd8c
Viniciusss065 <gomesfigueiredo@gmail.com>
2024-05-02 16:31:26 -0400

    exemplo parselnt js

--- tipo-dados.js
+++ tipo-dados.js
@@ -1,13 +1,14 @@
-1   let nome = "Wanderson";
-2   let verdadeiro = true;
-3   let falso = false;
-4   let numero = 40;
-5   let reais 25.99;
-6   let nulο = null;
-7   let indefinido;
-8   let NotANumber = 0 / 0;
-9   let objeto = {
-10     name: "Wanderson",
-11     age: 40,
-12     Profession: "Teacher"
-13  }
+let nome = "Wanderson";
+let verdadeiro = true;
+let falso = false;
+let numero = 40;
+let reais 25.99;
+let nulο = null;
+let indefinido;
+let NotANumber = 0 / 0;
+let objeto = {
+    name: "Wanderson",
+    age: 40,   
+    Profession: "Teacher"
+
+}
